84539f3239554a83878386879baf477b4d447eea
manu020766 <manu020766@yahoo.es>
2020-06-18 10:40:15 +0200

    add material module

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core'
+import { MatCardModule } from '@angular/material/card'
+import {MatButtonModule} from '@angular/material/button'
+import {MatInputModule} from '@angular/material/input'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatMenuModule} from '@angular/material/menu'
+import {MatIconModule} from '@angular/material/icon'
+import {MatListModule} from '@angular/material/list'
+import {MatFormFieldModule} from '@angular/material/form-field'
+
+const componentes = [
+  MatCardModule,
+  MatButtonModule,
+  MatInputModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatIconModule,
+  MatListModule,
+  MatFormFieldModule
+]
+
+@NgModule({
+  imports: componentes,
+  exports: componentes
+})
+export class MaterialModule { }
