fc6b908b530c3cb01371094a87633e77605c5ff2
manu020766 <manu020766@yahoo.es>
2020-06-18 11:40:45 +0200

    add angular material navigation

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard.component';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
 
+import { MaterialModule } from '../shared/material.module'
 
+import { DashboardComponent } from './dashboard.component'
+import { NavigationComponent } from '../shared/navigation/navigation.component'
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [
+    DashboardComponent,
+    NavigationComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule,
+    MaterialModule
+  ],
+  exports: [DashboardComponent]
 })
 export class DashboardModule { }
+
---
+++ src/app/shared/navigation/navigation.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core'
+import { Observable, Subscription, fromEvent } from 'rxjs'
+import { Router } from '@angular/router'
+import { MatSidenav } from '@angular/material/sidenav'
+import { NavigationService } from './navigation.service'
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.less']
+})
+export class NavigationComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('sidenav', {static: true}) public sidenav: MatSidenav
+
+  resizeObservable$: Observable<Event>
+  resizeSubscription$: Subscription
+  open:boolean
+  style:string
+
+  constructor(private router:Router, public navigatioService: NavigationService) {
+    // this.navigatioService.setSidenav(this.sidenav)
+    this.getScreenSize()
+
+    this.resizeObservable$ = fromEvent(window, 'resize')
+    this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => this.getScreenSize())
+   }
+
+  getScreenSize(event?) {
+        const scrWidth = window.innerWidth
+
+        if (scrWidth > 1024) {
+          this.open = true
+          this.style = 'side'
+        } else {
+          this.open = false
+          this.style = 'push'
+        } 
+  }
+
+  ngOnDestroy() {
+    this.resizeSubscription$.unsubscribe()
+  }
+
+  ngOnInit(): void { }
+  
+  ngAfterViewInit () {
+    this.navigatioService.setSidenav(this.sidenav)
+   }
+
+  toggleSideNav() {
+    // this.sidenav.toggle()
+    this.navigatioService.toggle()
+  }
+
+  navega(ruta:string) {
+    this.toggleSideNav()
+    this.router.navigate([ruta])
+  }
+
+}
